2707ff85f23bc4816c86864ac25bb5683568ac2e
AcuteChedder <nubik8653@gmail.com>
2025-05-06 21:23:32 +0300

    add search

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import SiteNavigation from "./components/SiteNavigation.vue"
 
 </script>
 
 <template>
-  
+  <div class="flex flex-col min-h-screen font-roboto bg-primary">
+    <SiteNavigation />
 
-  <RouterView />
+    <RouterView />
+  </div>
 </template>
 
 <style scoped>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,33 @@
 <script setup>
+import { ref } from 'vue';
+import axios from 'axios';
 
+const searchQuery = ref("")
+const queryTimeout = ref(null)
+const mapboxAPIKey = "pk.eyJ1Ijoiam9obmtvbWFybmlja2kiLCJhIjoiY2t5NjFzODZvMHJkaDJ1bWx6OGVieGxreSJ9.IpojdT3U3NENknF6_WhR2Q"
+const mapboxSearchResults = ref(null)
+
+const getSearchResults = () => {
+  clearTimeout(queryTimeout.value)
+  queryTimeout.value = setTimeout(async () => {
+    if (searchQuery.value !== "") {
+      const result = await axios.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${searchQuery.value}.json?access_token=${mapboxAPIKey}&types=place`);
+      mapboxSearchResults.value = result.data.features
+      console.log(mapboxSearchResults.value)
+      return
+    }
+    mapboxSearchResults.value = null
+  }, 300)
+}
 </script>
 
 <template>
-  <main>
-    <h1 class=" text-5xl text-green-800">ndjflnsdndffdaffffffffffffffffsdfdsfdsfdssdff</h1>
+  <main class=" w-container text-white">
+    <div class=" pt-4 mb-8 relative">
+      <input v-model="searchQuery" @input="getSearchResults" type="text" placeholder="search for a city or state" class=" py-2 px-1 w-full bg-transparent border-b focus:border-secondary focus:outline-none focus:shadow-md duration-200">
+      <ul class=" absolute bg-secondary text-white w-full shadow-md py-2 px-1 top-[66px]" v-if="mapboxSearchResults">
+        <li v-for="searchResult in mapboxSearchResults" :key="searchResult.id" class="py-2 cursor-pointer">{{ searchResult.place_name }}</li>
+      </ul>
+    </div>
   </main>
 </template>
